04096cca47b1a76b9c4d0ab6edc6d998f4ea86a5
YeHoonJang <jangyh0420@gmail.com>
2025-06-27 20:35:58 +0900

    chore: subtle fix

--- src/inference/decision_predict.sh
+++ src/inference/decision_predict.sh
@@ -1,4 +1,4 @@
 python /home/yehoon/workspace/patent_invalidation_trial/src/inference/decision_predict.py \
 --config "/home/yehoon/workspace/patent_invalidation_trial/config/decision_predict.json" \
---model "claude" \
+--model "gemini" \
 --prompt "subdecision_type"
