5c5cb1a9d13d564cb6c6da3a585110fc0949715a
Chaeee <oochaewonoo@gmail.com>
2025-07-08 16:51:37 +0900

    feat(model): add GPT Batch inference pipeline

--- src/llms/llm_client.py
+++ src/llms/llm_client.py
@@ -1,4 +1,5 @@
 from .gpt_client import GPTClient
+from .gpt_batch_client import GPTBatchClient
 from .claude_client import ClaudeClient
 from .gemini_client import GeminiClient
 
@@ -12,5 +13,13 @@ def get_llm_client(model_name, api_key, **kwargs):
         return ClaudeClient(api_key, **kwargs)
     elif name == "gemini":
         return GeminiClient(api_key, **kwargs)
+    else:
+        raise ValueError(f"Unsupported model: {model_name}")
+
+def get_llm_batch_client(model_name, api_key, **kwargs):
+    name = model_name.lower()
+
+    if (name == "gpt") or (name == "gpt-o"):
+        return GPTBatchClient(api_key, **kwargs)
     else:
         raise ValueError(f"Unsupported model: {model_name}")
