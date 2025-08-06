eef28e5c2d44c1cc63e674ce7e5681f2ffdf6f97
Chaewon-Leee <cw001121@naver.com>
2025-08-06 21:42:30 +0900

    feat(model) :  add t5gemma model inference pipeline

--- src/llms/t5_client.py
+++ src/llms/t5_client.py
@@ -17,20 +17,22 @@ class T5Client:
     def load_model(
         self, device_map: str = "cuda", model_config: dict = {}, cache_dir: str = None
     ):
-        config = AutoConfig.from_pretrained(self.model_name, **model_config)
-
         torch_dtype = model_config.pop("torch_dtype", None)
         if torch_dtype and isinstance(torch_dtype, str) and torch_dtype != "auto":
             torch_dtype = getattr(torch, torch_dtype)
 
-        tokenizer = AutoTokenizer.from_pretrained(self.model_name,
-                                                  **model_config)
+        tokenizer = AutoTokenizer.from_pretrained(
+            self.model_name,
+            trust_remote_code=True,
+            **model_config
+        )
         if "cuda" in device_map or "cpu" in device_map:
-            model = AutoModelForCausalLM.from_pretrained(
+            model = AutoModelForSeq2SeqLM.from_pretrained(
                 self.model_name,
                 torch_dtype=torch_dtype,
                 device_map=device_map,
-                config=config,
+                **model_config,
+                trust_remote_code=True,
                 **({ "cache_dir": cache_dir } if cache_dir else {}),
             )
 
@@ -38,25 +40,30 @@ class T5Client:
         self.model = model
 
     async def _call(self, prompt):
-        messages = [
-            {"role": "system", "content": prompt["system"]},
-            {"role": "user", "content": prompt["user"]},
-        ]
+        if self.model_name.lower().startswith("google/flan"):
+            input_text = prompt["user"]
 
-        input_ids = self.tokenizer.apply_chat_template(
-            messages,
-            return_tensors="pt",
-            return_dict=True,
-            add_generation_prompt=True
-        ).to(self.model.device)
+            input_ids = self.tokenizer(
+                input_text,
+                return_tensors="pt",
+                truncation=True,
+                max_length=self.tokenizer.model_max_length
+            ).to(self.model.device)
+        elif self.model_name.lower().startswith("google/t5gemma"):
+            messages = [
+                # {"role": "system", "content": prompt["system"]}, # system role 지원 X
+                {"role": "user", "content": prompt["user"]},
+            ]
 
-        pdb.set_trace()
+            input_ids = self.tokenizer.apply_chat_template(
+                messages,
+                return_tensors="pt",
+                return_dict=True,
+                add_generation_prompt=True
+            ).to(self.model.device)
 
-        response = self.model.generate(input_tensor.to(model.device), max_new_tokens=100)
-        response = model.generate(**input_ids, max_new_tokens=32)
-
-        generated_tokens = response[0][input_tensor.shape[1]:]
-        return self.tokenizer.decode(generated_tokens, skip_special_tokens=True)
+        response = self.model.generate(**input_ids, max_new_tokens=100)
+        return self.tokenizer.decode(response[0], skip_special_tokens=True)
 
     async def generate_valid_json(self, prompt: str) -> dict:
         retry_count = 0
