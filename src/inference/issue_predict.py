e39922092f8e0bccc57d7f510d11a5241e8e617d
YeHoonJang <jangyh0420@gmail.com>
2025-08-26 21:38:49 +0900

    feat(decision_predict, issue_predict, board_ruling_predict): add split+claim setting of issue_type & board_rulings & subdecision & subdecision_coarse

--- src/inference/issue_predict.py
+++ src/inference/issue_predict.py
@@ -1,7 +1,5 @@
 import argparse
 import asyncio
-import atexit
-import glob
 import json
 import os
 import sys
@@ -19,19 +17,137 @@ PROJECT_ROOT = Path(__file__).resolve().parents[2]
 sys.path.insert(0, str(PROJECT_ROOT / "src"))
 
 from utils.config_utils import load_config
-from llms.llm_client import get_llm_client
+from llms.llm_client import get_llm_client, get_llm_batch_client
 
 
-async def predict_issue_type(path, system_prompt, base_prompt, client, output_dir, model, stats, lock):
+def batch_process_file(files, system_prompt, base_prompt, output_dir, batch_path, uspto_path, client, model, stats, batch_id=None):
+    if not batch_id:
+        lines = []
+        skipped = 0
+        for p in files:
+            app_json = find_applicant_json(uspto_path, p.stem)
+            if app_json is None:
+                skipped += 1
+                continue
+            else:
+                app_patent = json.loads(app_json.read_text(encoding="utf-8"))
+
+            data = json.loads(p.read_text(encoding="utf-8"))
+
+            appellant = data["appellant_arguments"]
+            examiner = data["examiner_findings"]
+
+            if args.input_setting == "base":
+                full_prompt = base_prompt.format(
+                    appellant=appellant,
+                    examiner=examiner,
+                )
+            elif args.input_setting == "merge":
+                arguments = []
+                arguments.extend(appellant)
+                arguments.extend(examiner)
+
+                full_prompt = base_prompt.format(
+                    arguments=arguments,
+                )
+            elif args.input_setting == "split-claim":
+                full_prompt = base_prompt.format(
+                    appellant=appellant,
+                    examiner=examiner,
+                    app_claims = app_patent["claims"],
+                )
+
+            prompt = {
+                "system": system_prompt,
+                "user": full_prompt
+            }
+            lines.append(
+                client.make_request_line(prompt=prompt, custom_id=p.stem)
+            )
+        batch_path.write_text(
+            "\n".join(json.dumps(l, ensure_ascii=False) for l in lines) + "\n",
+            encoding="utf-8"
+        )
+        print(f"Created batch file {batch_path.name} with {len(lines)} requests (skipped {skipped})")
+
+        if "gpt" in model:
+            batch_id = client(batch_path)
+        elif "claude" in model:
+            batch_id = client(lines)
+        elif "gemini" in model:
+            batch_id = client(batch_path)
+
+    if not batch_id:
+        raise RuntimeError(f"batch id가 확인되지 않았습니다.")
+
+    if "gpt" in model and not batch_id.startswith("batch_"):
+        raise RuntimeError(f"[check] model has {model}, but batch_id={batch_id} startswith_batch={(batch_id or '').startswith('batch_')}")
+    elif "claude" in model and not batch_id.startswith("msgbatch_"):
+        raise RuntimeError(f"[check] model has {model}, but batch_id={batch_id} startswith_batch={(batch_id or '').startswith('msgbatch_')}")
+    elif "gemini" in model and not batch_id.startswith("batches/"):
+        raise RuntimeError(f"[check] model has {model}, but batch_id={batch_id} startswith_batch={(batch_id or '').startswith('batches')}")
+
+    validated = client.generate_valid_json(batch_id)
+
+    for filename, v in validated.items():
+        json_result = v.get("result", "")
+        input_token = v.get("input_token", 0)
+        cached_token = v.get("cached_token", 0)
+        output_token = v.get("output_token", 0)
+        reasoning_token = v.get("reasoning_token", 0)
+
+        if json_result:
+            output_path = output_dir/Path(filename).with_suffix(".json")
+            output_path.write_text(json.dumps(json_result, indent=2), encoding="utf-8")
+
+            wandb.log({
+                "name": filename,
+                "status": "ok" if json_result else "parse_fail",
+                "input_tokens": input_token or -1,
+                "cached_tokens": cached_token or -1,
+                "output_token": output_token or -1,
+                "reasoning_token": reasoning_token or -1,
+                "latency_ms": 0
+            })
+
+        stats["processed"] += 1
+        if json_result:
+            stats["succeeded"] += 1
+        else:
+            stats["failed"] += 1
+        if input_token: stats["sum_input_tokens"] += input_token
+        if cached_token: stats["sum_cached_tokens"] += cached_token
+        if output_token: stats["sum_output_tokens"] += output_token
+        if reasoning_token: stats["sum_reasoning_tokens"] += reasoning_token
+        stats["sum_latency_ms"] += 0
+
+
+async def predict_issue_type(args, path, system_prompt, base_prompt, app_patent, client, output_dir, model, stats, lock):
 
     data = json.loads(path.read_text(encoding="utf-8"))
+
     appellant = data["appellant_arguments"]
     examiner = data["examiner_findings"]
 
-    full_prompt = base_prompt.format(
-        appellant=appellant,
-        examiner=examiner
-    )
+    if args.input_setting == "base":
+        full_prompt = base_prompt.format(
+            appellant=appellant,
+            examiner=examiner,
+        )
+    elif args.input_setting == "merge":
+        arguments = []
+        arguments.extend(appellant)
+        arguments.extend(examiner)
+
+        full_prompt = base_prompt.format(
+            arguments=arguments,
+        )
+    elif args.input_setting == "split-claim":
+        full_prompt = base_prompt.format(
+            appellant=appellant,
+            examiner=examiner,
+            app_claims = app_patent["claims"]
+        )
 
     prompt = {
         "system": system_prompt,
@@ -47,11 +163,10 @@ async def predict_issue_type(path, system_prompt, base_prompt, client, output_di
                 print(f"[BLOCKED] {path.name}")
                 (output_dir / "blocked.log").open("a").write(f"{path.name}\n")
                 return
-        
+
         # get input/output tokens
         response, input_token, cached_token, output_token, reasoning_token = await client.generate_valid_json(prompt)
         latency_ms = round((time.perf_counter() - t0) * 1000)
-
 
         result = {}
         json_result = None
@@ -115,19 +230,23 @@ async def predict_issue_type(path, system_prompt, base_prompt, client, output_di
             stats["sum_latency_ms"] += latency_ms
 
 
+def find_applicant_json(uspto_root: Path, stem: str):
+    d = uspto_root / stem / "ApplicantPatent"
+    matches = list(d.glob("*.json"))
+    return matches[0] if matches else None
+
+
 def main(args):
     config = load_config(args.config)
     root_path = Path(config["path"]["root_path"])
+    uspto_path = root_path / config["path"]["uspto_dir"]
 
     prompt_dir = root_path / config["prompt"]["prompt_dir"]
     system_prompt_path = prompt_dir / config["prompt"]["system"]
     user_prompt_path = prompt_dir / config["prompt"][args.prompt]
 
-    with open(system_prompt_path, "r") as f:
-        system_prompt = f.read()
-
-    with open(user_prompt_path, "r") as f:
-        base_prompt = f.read()
+    system_prompt = system_prompt_path.read_text(encoding="utf-8")
+    base_prompt = user_prompt_path.read_text(encoding="utf-8")
 
     load_dotenv(PROJECT_ROOT / "config" / ".env")
     model = args.model.lower()
@@ -149,15 +268,13 @@ def main(args):
 
     if (use_api) and (not api_key):
         raise RuntimeError(f"환경변수 {model.upper()}_API_KEY가 설정되지 않았습니다.")
-    
+
     input_dir = root_path / config["path"]["input_dir"]
-    opinion_split_version = input_dir.parent.name
 
     output_dir = root_path / config["path"]["output_dir"] / args.prompt / config[model]["llm_params"]["model"]
     output_dir.mkdir(parents=True, exist_ok=True)
 
     llm_params = config[model]["llm_params"]
-
     mode = args.mode.lower()
     batch_id = args.batch_id
 
@@ -176,7 +293,7 @@ def main(args):
         raise ValueError(f"Unsupported mode: {mode}")
 
     all_files = input_dir.glob("*.json")
-    files = [p for p in all_files if not (output_dir / f"{p.name}").exists()][:3000]
+    files = [p for p in all_files if not (output_dir / f"{p.name}").exists()]
 
     run_name = f"{args.wandb_task}_{config[model]['llm_params']['model']}_{args.prompt}"
 
@@ -266,29 +383,42 @@ def main(args):
     for s in (signal.SIGINT, signal.SIGTERM):
         signal.signal(s, handle_sig)
 
-    sem = asyncio.Semaphore(config["async"]["concurrency"])
-    lock = asyncio.Lock()
+    if mode == "async":
+        sem = asyncio.Semaphore(config["async"]["concurrency"])
+        lock = asyncio.Lock()
 
-    async def sem_task(path):
-        async with sem:
-            await predict_issue_type(path, system_prompt, base_prompt, client, output_dir, model, stats, lock)
+        async def sem_task(path):
+            app_json = find_applicant_json(uspto_path, path.stem)
+            if app_json is None:
+                return
+            else:
+                app_patent = json.loads(app_json.read_text(encoding="utf-8"))
 
-    print(f"[Async] {config[model]['llm_params']['model']}")
-    asyncio.run(tqdm_asyncio.gather(*[sem_task(p) for p in files], desc="Predict Issue Type ..."))
+            async with sem:
+                await predict_issue_type(args, path, system_prompt, base_prompt, app_patent, client, output_dir, model, stats, lock)
+
+        print(f"[Async] {config[model]['llm_params']['model']}_{args.input_setting}")
+        asyncio.run(tqdm_asyncio.gather(*[sem_task(p) for p in files], desc="Predict Issue Type ..."))
+
+    elif mode == "batch":
+        batch_process_file(files, system_prompt, base_prompt, output_dir, batch_path, uspto_path, client, model, stats, batch_id)
 
     EXIT_REASON = "completed"
     finalize(end_run=True)
-    
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--config", type=str, required=False, default="config/issue_predict.json", help="Path of configuration file (e.g., issue_predict.json)")
-    parser.add_argument("--model", choices=["gpt", "gpt-o", "claude", "gemini", "llama", "qwen", "solar", "mistral", "deepseek", "t5"], required=False, default="gpt", help="LLM Model for issue type prediction")
+    parser.add_argument("--model", choices=["gpt", "gpt-batch", "gpt-o", "gpt-o-batch", "claude", "gemini", "gemini-batch", "llama", "qwen", "solar", "mistral", "deepseek", "t5"], required=False, default="gpt", help="LLM Model for issue type prediction")
     parser.add_argument("--prompt", type=str, required=True, default=None, help="Prompt for inferencing")
     parser.add_argument("--wandb_entity", default="patent_project")
     parser.add_argument("--wandb_project", default="issue_type_predict")
     parser.add_argument("--wandb_task", default="issue_type_predict")
+    parser.add_argument("--input_setting", type=str, required=True, choices=["base", "merge", "split-claim", "claim-only"], default="base", help="Input setting")
+    parser.add_argument("--mode", choices=["async", "batch"], required=True, default="async", help="Mode for issue type prediction")
+    parser.add_argument("--batch_id", required=False, default=None, help="batch id")
 
     args = parser.parse_args()
 
